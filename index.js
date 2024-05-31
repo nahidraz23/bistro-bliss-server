c0fd890f524b81b035cbba7704073e0089dd12a0
nahidraz23 <nzamanraz@gmail.com>
2024-06-01 01:40:56 +0600

    get FoodItem by id api implemented

--- index.js
+++ index.js
@@ -125,6 +125,13 @@ async function run () {
       res.send(result)
     })
 
+    app.get('/menu/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await foodItemsColletection.findOne(query);
+      res.send(result);
+    })
+
     app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
       const foodItem = req.body
       const result = await foodItemsColletection.insertOne(foodItem)
